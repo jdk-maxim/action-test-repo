90d286e1ec9a31a516c390446fba5090e9f9b5f2
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-10-07 17:14:23 -0500

    Remove git requirements from push_actions.sh

--- push_actions.sh
+++ push_actions.sh
@@ -1,27 +1,18 @@
 #!/bin/sh -l
 
 echo "Staring push actions"
-ls
 echo "Running on OS: $(uname -a)"
-git status
-git log -n 3
 which python3
 python3 --version
-pylint --version
-
-
-# Install required packages
-# TODO: Will replace with docker in future
-#apt-get install git
-#apt-get install python
-#sudo apt install python3-venv python3-pip
-
-# all pip missing from ubuntu version: atomicwrites numpy onnx packagin pluggy protobuf py pylint pyparsing pytest
 pip3 --version
 
 pip3 install Cython 
+pip3 install pylint
+
+pylint --version
 
 pip3 install -r requirements.txt 
+
 if [ $? != 0 ]
 then
 	echo "Failed to install requirements.txt"
@@ -36,7 +27,7 @@ then
 	echo "Failed to install requirements_distiller.txt"
 	exit 1
 else
-	echo "requirements_distiller.txt intall passed" 
+	echo "requirements_distiller.txt install passed"
 fi
 
 #git diff master | grep "diff --" | grep "\.py$" | awk '{ print $4 }' | sed "s/^b/\./" | xargs pylint
